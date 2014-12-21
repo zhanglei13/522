5eb8b91ad57966cb3750247e10aebfbbe7978d5a
highya <827931245@qq.com>
2014-12-21 13:44:16 +0800

    3 sum closest

--- leetcode/2014.12.22/threeSumCloset/highya.py
+++ leetcode/2014.12.22/threeSumCloset/highya.py
@@ -1,4 +1,17 @@
-def 3sumCloset(num,target):
+def threeSumCloset(num,target):
     num.sort()
-    
-    
+    ans=None
+    for i in range(len(num)):
+        l,r=i+1,len(num)-1
+        while l<r:
+            sum=num[i]+num[l]+num[r]
+            if ans is None or abs(sum-target)<ans:
+                ans=abs(sum-target)
+                res=sum
+            if sum<target:
+                l+=1
+            elif sum>target:
+                r-=1
+            else:
+                return sum
+    return res
